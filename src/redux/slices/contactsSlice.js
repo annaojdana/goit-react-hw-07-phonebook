bbf8bb7df3210212d6d1844d2632b8f08e38ad99
Anna Ojdana <anna.ojdana@gmail.com>
2022-10-05 14:25:12 +0200

    contactsSlice reducer

--- src/redux/slices/contactsSlice.js
+++ src/redux/slices/contactsSlice.js
@@ -1,6 +1,36 @@
 import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import axios from "axios";
+
+const BASIC_URL = 'https://63396c6866857f698fb5d11c.mockapi.io/api/v1/contacts';
 
 
-const BASIC_URL = 'https://63396c6866857f698fb5d11c.mockapi.io/api/v1/';
+const initialState = [];
+
+export const fetchContacts = createAsyncThunk('contacts/fetchContacts', async () => {
+  try {
+    const response = await axios.get(BASIC_URL);
+    console.log(response);
+    return [...response];
+  }
+  catch (error) {
+    return error.message;
+  }
+})
 
 
+const contactsSlice = createSlice({
+  name: 'contacts',
+  initialState,
+  reducers: {}
+})
+
+
+
+
+export const selectAllPosts = state => state.posts.posts;
+export const getPostsStatus = state => state.posts.status;
+export const getPostsError = state => state.posts.error;
+
+export const { postAdded, reactionAdded } = postsSlice.actions;
+
+export default contactsSlice.reducer;
