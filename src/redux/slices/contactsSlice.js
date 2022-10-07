b3364216ec4569431631270462dac33945cb1c1a
Anna Ojdana <anna.ojdana@gmail.com>
2022-10-07 12:40:14 +0200

    extraReducers with fetchContacts

--- src/redux/slices/contactsSlice.js
+++ src/redux/slices/contactsSlice.js
@@ -1,36 +1,47 @@
-import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
-import axios from "axios";
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
+import axios from 'axios';
 
 const BASIC_URL = 'https://63396c6866857f698fb5d11c.mockapi.io/api/v1/contacts';
 
-
 const initialState = [];
 
-export const fetchContacts = createAsyncThunk('contacts/fetchContacts', async () => {
-  try {
-    const response = await axios.get(BASIC_URL);
-    console.log(response);
-    return [...response];
+export const fetchContacts = createAsyncThunk(
+  'contacts/fetchContacts',
+  async () => {
+    try {
+      const response = await axios.get(BASIC_URL);
+      console.log(response);
+      return [...response];
+    } catch (error) {
+      return error.message;
+    }
   }
-  catch (error) {
-    return error.message;
-  }
-})
+);
 
+export const addNewContact = createAsyncThunk(
+  'contacts/addNewContact',
+  async initialContact => {
+    const response = await axios.post(BASIC_URL, initialContact);
+    return response;
+  }
+);
 
 const contactsSlice = createSlice({
   name: 'contacts',
   initialState,
-  reducers: {}
-})
+  reducers: {
+  },
+  extraReducers(builder) {
+    builder.addCase(fetchContacts.fulfilled, (state, { payload }) => {
+      state.contacts.push(payload);
+    });
+  },
+});
 
+export const getAllContacts = state => state.contacts;
+export const getContactsStatus = state => state.status;
+export const getContactsError = state => state.error;
 
-
-
-export const selectAllPosts = state => state.posts.posts;
-export const getPostsStatus = state => state.posts.status;
-export const getPostsError = state => state.posts.error;
-
-export const { postAdded, reactionAdded } = postsSlice.actions;
+export const { contactAdded, reactionAdded } = contactsSlice.actions;
 
 export default contactsSlice.reducer;
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,11 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit';
-import contactsSlice from './slices/contactsSlice';
+import contactsReducer from './slices/contactsSlice';
 import filterSlice from './slices/filterSlice';
 
 const store = configureStore({
   reducer: {
     filter: filterSlice,
-    contacts: contactsSlice,
+    contacts: contactsReducer,
   },
 });
 
