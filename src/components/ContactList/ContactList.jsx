fc0298a9b546f0c5429275e5c80d1d06da582f3f
Anna Ojdana <anna.ojdana@gmail.com>
2022-10-08 10:23:49 +0200

    create variable content

--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -4,19 +4,62 @@ import React from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { useEffect } from 'react';
 
+import {
+  selectAllContacts,
+  getContactsError,
+  getContactsStatus,
+  fetchContacts,
+} from 'redux/slices/contactsSlice';
+
 const ContactList = () => {
   const { wrapper, text, button } = styles;
 
-  const contacts = useSelector(state => state.contacts);
-  const filter = useSelector(state => state.filter);
+  const dispatch = useDispatch();
 
+  const contacts = useSelector(selectAllContacts);
+  const contactsStatus = useSelector(getContactsStatus);
+  const error = useSelector(getContactsError);
+  const filter = useSelector(state => state.filter);
   const filteredContacts = contacts.filter(c =>
     c.name.toLowerCase().includes(filter)
   );
 
-  const dispatch = useDispatch();
+  useEffect(() => {
+    if (contactsStatus === 'idle') {
+      dispatch(fetchContacts());
+    }
+  }, [contactsStatus, dispatch]);
+  
+  console.log(contacts);
 
- 
+  let content;
+  if (contactsStatus === 'loading') {
+    content = <p>"Loading..."</p>;
+  } else if (contactsStatus === 'succeeded') {
+    console.log(contacts[1]);
+    content = (
+      <ul className={wrapper}>
+        {filteredContacts.map(contact => {
+          return (
+            <li className={text} key={contact.id}>
+              <span>{`${contact.name}: ${contact.number}`}</span>
+              <button
+                type="button"
+                className={button}
+                // onClick={() => deleteItemContact(contact.id)}
+              >
+                Delete
+              </button>
+            </li>
+          );
+        })}
+      </ul>
+    );
+  } else if (contactsStatus === 'failed') {
+    content = <p>{error}</p>;
+  }
+
+
 
   console.log(contacts);
   // const deleteItemContact = id => {
@@ -26,22 +69,7 @@ const ContactList = () => {
   return (
     <>
       {filteredContacts.length > 0 ? (
-        <ul className={wrapper}>
-          {filteredContacts.map(contact => {
-            return (
-              <li className={text} key={contact.id}>
-                <span>{`${contact.name}: ${contact.number}`}</span>
-                <button
-                  type="button"
-                  className={button}
-                  // onClick={() => deleteItemContact(contact.id)}
-                >
-                  Delete
-                </button>
-              </li>
-            );
-          })}
-        </ul>
+        {content}
       ) : (
         <Notification message="You don't have this contact" />
       )}
